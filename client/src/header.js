42bfc157710256b40338e22325170a34636a8106
Aman Kharbanda <amankharbanda2002@gmail.com>
2024-01-05 17:03:35 +0530

    kkk

--- client/src/header.js
+++ client/src/header.js
@@ -1,45 +1,46 @@
-import { useContext, useEffect,useState } from "react";
 import {Link} from "react-router-dom";
-import { UserContext } from "./UserContext";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
 
-export default function Header(){
-//   const [username,setUsername] = useState(null);
-    const {setUserInfo,userInfo} = useContext(UserContext);
-    useEffect(()=>{
-      fetch('http://localhost:4000/profile',{
-        credentials:'include',
-      }).then(response => {
-        response.json().then(userInfo => {
-            setUserInfo(userInfo);
-        });
-      })
-    },[]);
-
-    function logout(){
-      fetch('http://localhost:4000/logout',{
-        credentials:'include',
-        method:'POST',
+export default function Header() {
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
       });
-      setUserInfo(null);
-    }
-    const username = userInfo?.username;
-    return (
-        <header>
-        <Link to="/" className = "logo">My Blog</Link>
-        <nav>
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">Bike Selling App</Link>
+      <nav>
         {username && (
           <>
-            <Link to="/create">Create new Post</Link>
-            <a onClick={logout}>Logout</a>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
           </>
         )}
         {!username && (
           <>
-            <Link to='/login'>Login</Link>
-            <Link to='/register'>Register</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
           </>
         )}
-        </nav>
-      </header>
-    );
+      </nav>
+    </header>
+  );
 }
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,39 +1,33 @@
-import {useState} from "react";    //We initialize our state by calling useState in our function component.
+import {useState} from "react";
 
-export default function RegisterPage(){
-    const [username, setUsername] = useState('');    // usestate is used to initialize it and it return two state current and update state
-    const [password, setPassword] = useState('');
-
-    async function register(ev){   //The keyword async before a function makes the function return a promise
-        ev.preventDefault();
-        const response = await fetch('http://localhost:4000/register',{   //The await keyword makes the function pause the execution and wait for a resolved promise before it continues
-            method:'POST',
-            body: JSON.stringify({username,password}),
-            headers: {'Content-Type' : 'application/json'},
-        });
-        // console.log(response);
-        if(response.status === 200){
-            alert('registration successful');
-        }
-        else{
-            alert('registration failed');
-        }
+export default function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+    });
+    if (response.status === 200) {
+      alert('registration successful');
+    } else {
+      alert('registration failed');
     }
-    
-    // onchange event detects the change in value
-    return(
+  }
+  return (
     <form className="register" onSubmit={register}>
-        <h1>Register</h1>
-            <input type="text" 
-                placeholder="username" 
-                value={username}
-                onChange={ev => setUsername(ev.target.value)}></input>   
-            <input type="password" 
-                placeholder="password"
-                value={password}
-                onChange={ev => setPassword(ev.target.value)}></input>
-            <button>Register</button>
+      <h1>Register</h1>
+      <input type="text"
+             placeholder="username"
+             value={username}
+             onChange={ev => setUsername(ev.target.value)}/>
+      <input type="password"
+             placeholder="password"
+             value={password}
+             onChange={ev => setPassword(ev.target.value)}/>
+      <button>Register</button>
     </form>
-    );
+  );
 }
-
